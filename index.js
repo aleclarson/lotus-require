a3524a9a67786549d23794f579e105efeaa7d044
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-05-23 11:35:12 -0700

    05/23/2016 11:35 AM

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ var LOTUS = Symbol.for('lotus');
 var lotus = global[LOTUS];
 
 if (!lotus) {
-  global[LOTUS] = lotus = require('./js/src/index');
+  var lotusPath = process.env.LOTUS_PATH;
+
+  // Try using the local version.
+  if (lotusPath) {
+    lotusPath += '/lotus-require';
+    if (__dirname === lotusPath) {
+      // We are already using the local version.
+    }
+    else if (require('fs').existsSync(lotusPath)) {
+      lotus = require(lotusPath);
+    }
+  }
+
+  // Default to using the installed remote version.
+  if (!lotus) {
+    lotus = require('./js/src/index');
+  }
+
+  global[LOTUS] = lotus;
 }
 
 module.exports = lotus;
